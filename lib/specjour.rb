7b5078f7c17e4108c506ffb78a14e2cf3a50d563
Sandro Turriate <sandro.turriate@gmail.com>
2010-02-15 01:22:27 -0500

    Rsync Daemon

--- lib/specjour.rb
+++ lib/specjour.rb
@@ -1,5 +1,5 @@
 module Specjour
-  class Server
+  class Dispatcher
     attr_reader :project_path
 
     def initialize
@@ -14,31 +14,83 @@ module Specjour
       File.basename(project_path)
     end
 
-    def prepare
-      system("rsync", *rsync_options)
+    def serve
+      rsync_daemon.start
     end
 
     protected
 
-    def rsync_destination
-      "/tmp/"
-    end
-
-    def rsync_options
-      %w(-avv --delete --delete-excluded) + rsync_exclusions + [project_path, rsync_destination]
-    end
-
-    def rsync_exclusions
-      %w(--exclude=.git --exclude=doc --exclude=tmp/* --exclude=public --exclude=log --exclude=script)
+    def rsync_daemon
+      @rsync_daemon ||= RsyncDaemon.new(project_path, project_name)
     end
   end
 
-  class Client
+  class RsyncDaemon
+    require 'fileutils'
 
-    def sync
-      "rsync -az -e 'ssh -l dev' farnsworth.local:/tmp/workbeast ."
+    attr_reader :project_path, :project_name
+    def initialize(project_path, project_name)
+      @project_path = project_path
+      @project_name = project_name
     end
 
+    def config_file
+      File.join(project_path, "tmp", "rsyncd.conf")
+    end
+
+    def start
+      write_config
+      system("rsync", "--daemon", "--config=#{config_file}", "--port=8989")
+    end
+
+    def stop
+      Process.kill("TERM", pid)
+      FileUtils.rm(pid_file)
+    end
+
+    protected
+
+    def write_config
+      unless File.exists?(config_file)
+        File.open(config_file, 'w') do |f|
+          f.write config
+        end
+      end
+    end
+
+    def pid
+      File.read(pid_file).strip.to_i
+    end
+
+    def pid_file
+      File.join("/tmp", "#{project_name}_rsync_daemon.pid")
+    end
+
+    def config
+      <<-CONFIG
+# global configuration
+use chroot = no
+timeout = 60
+read only = yes
+pid file = #{pid_file}
+
+[#{project_name}]
+  path = #{project_path}
+  exclude = .git* doc tmp/* public log script
+      CONFIG
+    end
+  end
+
+  class Worker
+    attr_reader :project_path, :project_name
+    def initialize(project_path, project_name)
+      @project_path = project_path
+      @project_name = project_name
+    end
+
+    def sync
+      "rsync -a --delete --port=8989 santurimob.local::#{project_name} /tmp/#{project_name}"
+    end
   end
 end
 
