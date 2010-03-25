cbb5f5b3609ade98e9f9b7f26237948ad1df67d9
Sandro Turriate <sandro.turriate@gmail.com>
2010-03-25 17:22:43 -0400

    Disable referential integrity when deleting tables

--- lib/specjour/db_scrub.rb
+++ lib/specjour/db_scrub.rb
@@ -20,15 +20,24 @@ module Specjour
 
     protected
 
+    def connection
+      ActiveRecord::Base.connection
+    end
+
     def purge_tables
-      tables = ActiveRecord::Base.connection.tables - ['schema_migrations']
-      tables.each do |table|
-        ActiveRecord::Base.connection.delete "delete from #{table}"
+      connection.disable_referential_integrity do
+        tables_to_purge.each do |table|
+          connection.delete "delete from #{table}"
+        end
       end
     end
 
     def pending_migrations?
       ActiveRecord::Migrator.new(:up, 'db/migrate').pending_migrations.any?
+    end
+
+    def tables_to_purge
+      connection.tables - ['schema_migrations']
     end
   end
 end
