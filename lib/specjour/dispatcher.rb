4acae7b40aa35f876206f3ea7b31e5305c003da7
Sandro Turriate <sandro.turriate@gmail.com>
2010-04-13 19:56:09 -0400

    Don't work with slow managers
    When a manager hangs, the remaining bonjour replies never get through.
    Then the timeout is reached, the slow manager is thrown out and we can
    continue processing other managers

--- lib/specjour/dispatcher.rb
+++ lib/specjour/dispatcher.rb
@@ -40,12 +40,16 @@ module Specjour
     end
 
     def fetch_manager(uri)
-      manager = DRbObject.new_with_uri(uri.to_s)
-      if !managers.include?(manager) && manager.available_for?(project_name)
-        set_up_manager(manager, uri)
-        managers << manager
-        self.worker_size += manager.worker_size
+      Timeout.timeout(1) do
+        manager = DRbObject.new_with_uri(uri.to_s)
+        if !managers.include?(manager) && manager.available_for?(project_name)
+          set_up_manager(manager, uri)
+          managers << manager
+          self.worker_size += manager.worker_size
+        end
       end
+    rescue Timeout::Error
+      Specjour.logger.debug "Couldn't work with manager at #{uri}"
     end
 
     def gather_managers
