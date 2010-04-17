fd43e0dfc0c0862da9a5043b3499836332294375
Sandro Turriate <sandro.turriate@gmail.com>
2010-04-17 11:17:44 -0400

    Use grep as it was intended, no more piping!

--- lib/specjour/cpu.rb
+++ lib/specjour/cpu.rb
@@ -1,12 +1,12 @@
 module Specjour
   module CPU
-    # copied from github.com/grosser/parallel
+    # inspired by github.com/grosser/parallel
     def self.cores
       case RUBY_PLATFORM
       when /darwin/
         `hwprefs cpu_count`.to_i
       when /linux/
-        `cat /proc/cpuinfo | grep --count processor`.to_i
+        `grep --count processor /proc/cpuinfo`.to_i
       end
     end
   end
