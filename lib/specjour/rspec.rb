b8b7e1be35a47736f5e782873133f3bc78bed1ab
Sandro Turriate <sandro.turriate@gmail.com>
2010-04-20 21:34:54 -0400

    Autoloading causes more trouble than it's worth

--- lib/specjour/rspec.rb
+++ lib/specjour/rspec.rb
@@ -3,8 +3,8 @@ module Specjour
     require 'spec'
     require 'spec/runner/formatter/base_text_formatter'
 
-    autoload :DistributedFormatter, 'specjour/rspec/distributed_formatter'
-    autoload :FinalReport, 'specjour/rspec/final_report'
-    autoload :MarshalableFailureFormatter, 'specjour/rspec/marshalable_failure_formatter'
+    require 'specjour/rspec/distributed_formatter'
+    require 'specjour/rspec/final_report'
+    require 'specjour/rspec/marshalable_failure_formatter'
   end
 end
