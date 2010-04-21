f6dc9c2a1cc452d81c5103b544d953e5e6163a99
Sandro Turriate <sandro.turriate@gmail.com>
2010-04-20 20:15:20 -0400

    Rename printer to connection

--- lib/specjour/worker.rb
+++ lib/specjour/worker.rb
@@ -22,28 +22,28 @@ module Specjour
     end
 
     def run
-      printer.send_message(:ready)
+      connection.send_message(:ready)
       run_time = 0
       Dir.chdir(project_path)
-      while !printer.closed? && data = printer.gets(TERMINATOR)
+      while !connection.closed? && data = connection.gets(TERMINATOR)
         test = load_object(data)
         if test
           run_time += Benchmark.realtime do
             run_test test
           end
-          printer.send_message(:ready)
+          connection.send_message(:ready)
         else
-          printer.send_message(:worker_summary=, {:duration => sprintf("%6f", run_time)})
-          printer.send_message(:done)
-          printer.disconnect
+          connection.send_message(:worker_summary=, {:duration => sprintf("%6f", run_time)})
+          connection.send_message(:done)
+          connection.disconnect
         end
       end
     end
 
     protected
 
-    def printer
-      @printer ||= printer_connection
+    def connection
+      @connection ||= printer_connection
     end
 
     def printer_connection
@@ -61,7 +61,7 @@ module Specjour
 
     def run_feature(feature)
       set_up_cucumber
-      cli = ::Cucumber::Cli::Main.new(['--format', 'Specjour::Cucumber::DistributedFormatter', feature], printer)
+      cli = ::Cucumber::Cli::Main.new(['--format', 'Specjour::Cucumber::DistributedFormatter', feature], connection)
       cli.execute!(::Cucumber::Cli::Main.step_mother)
     end
 
@@ -69,7 +69,7 @@ module Specjour
       options = Spec::Runner::OptionParser.parse(
         ['--format=Specjour::Rspec::DistributedFormatter', spec],
         $stderr,
-        printer
+        connection
       )
       Spec::Runner.use options
       options.run_examples
