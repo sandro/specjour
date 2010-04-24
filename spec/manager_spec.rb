3642a3f6f8a2e83d95c3932c64a7d43879feb4ef
Sandro Turriate <sandro.turriate@gmail.com>
2010-04-24 13:57:57 -0400

    Specs for Manager#available_for?

---
+++ spec/manager_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Specjour::Manager do
+  describe "#available_for?" do
+    it "is available for all projects by default" do
+      subject.available_for?(rand.to_s).should be_true
+    end
+
+    it "is available for one project" do
+      manager = Specjour::Manager.new :registered_projects => %w(one)
+      manager.available_for?('one').should be_true
+    end
+
+    it "is available for many projects" do
+      manager = Specjour::Manager.new :registered_projects => %w(one two)
+      manager.available_for?('one').should be_true
+      manager.available_for?('two').should be_true
+    end
+  end
+end
