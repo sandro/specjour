6a164aff3efacf3c091a0e7380f3d1b2d34ea9ef
Sandro Turriate <sandro.turriate@gmail.com>
2010-09-07 10:34:14 -0400

    Version bump

--- lib/specjour.rb
+++ lib/specjour.rb
@@ -28,7 +28,7 @@ module Specjour
   autoload :Cucumber, 'specjour/cucumber'
   autoload :Rspec, 'specjour/rspec'
 
-  VERSION = "0.3.0.rc5".freeze
+  VERSION = "0.3.0.rc6".freeze
   HOOKS_PATH = ".specjour/hooks.rb"
 
   class << self
