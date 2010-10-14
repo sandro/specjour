4a6aef06ad1b48a23bd941c0569f6e76057c257a
Sandro Turriate <sandro.turriate@gmail.com>
2010-10-14 12:59:40 -0400

    Bump version

--- lib/specjour.rb
+++ lib/specjour.rb
@@ -28,7 +28,7 @@ module Specjour
   autoload :Cucumber, 'specjour/cucumber'
   autoload :Rspec, 'specjour/rspec'
 
-  VERSION = "0.3.0.rc8".freeze
+  VERSION = "0.3.0".freeze
   HOOKS_PATH = "./.specjour/hooks.rb"
 
   def self.interrupted?
