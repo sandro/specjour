c7891b3bb399c033430b61d0be2ac26f77ca827b
Sandro Turriate <sandro.turriate@gmail.com>
2012-01-13 18:51:49 -0800

    Exclude specjour internals from rspec backtraces

--- lib/specjour/rspec/runner.rb
+++ lib/specjour/rspec/runner.rb
@@ -1,4 +1,5 @@
 module Specjour::RSpec::Runner
+  ::RSpec::Core::Runner::AT_EXIT_HOOK_BACKTRACE_LINE.replace "#{__FILE__}:#{__LINE__ + 3}:in `run'"
   def self.run(spec, output)
     args = ['--format=Specjour::RSpec::DistributedFormatter', spec]
     ::RSpec::Core::Runner.run args, $stderr, output
