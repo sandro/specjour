864c6f17e6a8ad58149d50d7585294c3a8061aa0
Sandro Turriate <sandro.turriate@gmail.com>
2012-02-21 23:06:59 -0800

    Bump

--- lib/specjour.rb
+++ lib/specjour.rb
@@ -29,7 +29,7 @@ module Specjour
   autoload :Cucumber, 'specjour/cucumber'
   autoload :RSpec, 'specjour/rspec'
 
-  VERSION ||= "0.5.1"
+  VERSION ||= "0.5.2"
   HOOKS_PATH ||= "./.specjour/hooks.rb"
   PROGRAM_NAME ||= $PROGRAM_NAME # keep a reference of the original program name
 
