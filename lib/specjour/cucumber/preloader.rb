e049dcde79aad8ad67f01a9b69c10408b155fd45
Sandro Turriate <sandro.turriate@gmail.com>
2012-10-16 10:02:30 -0700

    Benchmark load time of cucumber environment

--- lib/specjour/cucumber/preloader.rb
+++ lib/specjour/cucumber/preloader.rb
@@ -2,13 +2,14 @@ module Specjour
   module Cucumber
     module Preloader
       def self.load
-        $stdout.puts "Loading Cucumber Environment"
-        require 'cucumber' unless defined?(::Cucumber::Cli)
-        configuration = ::Cucumber::Cli::Configuration.new
-        configuration.parse! []
-        runtime = ::Cucumber::Runtime.new(configuration)
-        runtime.send :load_step_definitions
-        Cucumber.runtime = runtime
+        Specjour.benchmark("Loading Cucumber Environment") do
+          require 'cucumber' unless defined?(::Cucumber::Cli)
+          configuration = ::Cucumber::Cli::Configuration.new
+          configuration.parse! []
+          runtime = ::Cucumber::Runtime.new(configuration)
+          runtime.send :load_step_definitions
+          Cucumber.runtime = runtime
+        end
       end
     end
   end
