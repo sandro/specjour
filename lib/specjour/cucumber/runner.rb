aff24501ef147d3152d79da0fd0e90999ed977a1
pierre-o13 <pierre.ozoux@seedrs.com>
2012-10-25 13:03:14 +0100

    test

--- lib/specjour/cucumber/runner.rb
+++ lib/specjour/cucumber/runner.rb
@@ -3,14 +3,12 @@ module Specjour
     module Runner
       def self.run(feature_path)
         feature = ::Cucumber::FeatureFile.new(feature_path).parse(Cucumber.configuration.filters, {})
-
-        features = ::Cucumber::Ast::Features.new
-
-        features.add_feature(feature)
-
-
-
-
+        if feature
+          features = ::Cucumber::Ast::Features.new
+          features.add_feature(feature)
+        else 
+          debugger
+        end
         Cucumber.tree_walker.visit_features(features)
       end
     end
