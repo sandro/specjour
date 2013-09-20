dc6533e2ee466f37e56f0366aa58c6855cb32977
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-20 12:44:17 -0600

    Attempting to fix errors with dropping when no db exists.

--- lib/specjour.rb
+++ lib/specjour.rb
@@ -22,7 +22,7 @@ module Specjour
   autoload :Manager, 'specjour/manager'
   autoload :Printer, 'specjour/printer'
   autoload :Protocol, 'specjour/protocol'
-  autoload :Railtie, 'specjour/railtie'
+  require 'specjour/railtie'
   autoload :RsyncDaemon, 'specjour/rsync_daemon'
   autoload :SocketHelper, 'specjour/socket_helper'
   autoload :Worker, 'specjour/worker'
