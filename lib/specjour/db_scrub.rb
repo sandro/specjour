bd7d3ef6d69d883db1f3140df3e3e293ba0eba0d
Karen Lundgren <karen.e.lundgren@gmail.com>
2013-09-20 15:31:10 -0600

    Gah!

--- lib/specjour/db_scrub.rb
+++ lib/specjour/db_scrub.rb
@@ -56,11 +56,9 @@ module Specjour
 
     def schema_load_task
       if ActiveRecord::Base.schema_format == :sql
-        Rake::Task['db:test:load_structure'].invoke
+        Rake::Task['db:test:load'].invoke
       elsif ActiveRecord::Base.schema_format == :ruby
-        Rake::Task['db:schema:load'].invoke
-        ActiveRecord::Base.establish_connection
-        Rake::Task['db:migrate'].invoke
+        Rake::Task['db:test:load_structure'].invoke
       else
         raise 'Invalid schema format ' + ActiveRecord::Base.schema_format.to_s
       end
